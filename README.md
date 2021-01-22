6aaebb260560d4fd4aa5a3f549e2746c83577704
Andrii Mishtal <mishtal.andriy@gmail.com>
2021-01-22 17:35:59 +0200

    finish

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # scroll_addclassNavbar_
+[DEMO LInK](https://mishtal-andrii.github.io/scroll_addclassNavbar_menutoggle/)
--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
-// ********** set date ************
-// select span
+// ********** set date ************]
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
@@ -13,17 +12,78 @@ const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
+/* navToggle.addEventListener('click', () => {
+  linksContainer.classList.toggle('show-links')
+})
+ */
+// If you add more links we will dynamic calculate height of '.show-links'
+navToggle.addEventListener('click', () => {
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
 
+  if(containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  }
+  else {
+    linksContainer.style.height = 0;
+  }
+  
+})
 
 // ********** fixed navbar ************
 
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
 
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav')
+     navbar.classList.add('fixed-nav')
+  } else {
+    navbar.classList.remove('fixed-nav')
+  }
 
+  // Show bottom link
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link')
+  } else {
+    topLink.classList.remove('show-link')
+  }
+
+})
 
 // ********** smooth scroll ************
-// select links
+// select links hiyde mobile link when touch to link, scrool using javascrp
 const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    // navigate to specific place
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id)
+    // Calculate height 
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav')
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position -= navHeight
+    }
+    if (navHeight > 82) {
+      position += containerHeight
+    }
+
+    window.scrollTo({
+      left:0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  })
+})
 
 // calculate heights
